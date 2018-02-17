9794727a38f57c3527c12245a8a8513a5c5b87fc
tombjo13 <tbjrnerud@gmail.com>
2018-02-17 20:36:45 +0100

    Update bonetrousle.js

--- Don't-Give-Up-faster - Kopi/bonetrousle.js
+++ Don't-Give-Up-faster - Kopi/bonetrousle.js
@@ -20,7 +20,7 @@ function update() {
 
 	if (rate_timer <= 0) {
 		rate_timer += percent_time;
-		rate += 0.00015625;
+		rate += 0.0003125;
 		bonetrousle.rate(rate);
 		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(0) + "%";
 	}
