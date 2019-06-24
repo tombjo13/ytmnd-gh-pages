eb7e9058c54d8e8f542c4ac3c37b521b78c9c9e9
tombjo13 <tbjrnerud@gmail.com>
2019-06-24 02:38:36 +0200

    Add files via upload

---
+++ undyne-gh-pages/gh-pages/120 hz/bonetrousle.js
@@ -0,0 +1,37 @@
+var bonetrousle = new Howl({
+	src: ["audio/bonetrousle.ogg", "audio/bonetrousle.mp3"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 0.4032;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 1000;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.005;
+		bonetrousle.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(1) + "%";
+	}
+
+	document.getElementById("undyne").style.top = ((rate * 2) - Math.random() * rate * 4) + "px";
+	document.getElementById("undyne").style.left = ((rate * 2) - Math.random() * rate * 4) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	bonetrousle.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
