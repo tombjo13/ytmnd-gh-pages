0c9dbbbb13c59bebad46b85ad2360891b8999d9b
tombjo13 <tbjrnerud@gmail.com>
2019-09-04 03:52:11 +0200

    Add files via upload

---
+++ hopes-and-dreams/gh-pages/240hz/bonetrousle.js
@@ -0,0 +1,37 @@
+var bonetrousle = new Howl({
+	src: ["audio/Undertale_Ost_087_-_Hopes_and_Dreams.mp3"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 1.82184;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 250;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.0025;
+		bonetrousle.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(2) + "%";
+	}
+
+	document.getElementById("papyrus").style.top = (rate - Math.random() * rate * 2) + "px";
+	document.getElementById("papyrus").style.left = (rate - Math.random() * rate * 2) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	bonetrousle.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
